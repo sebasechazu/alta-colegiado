7b59a3e39f5438e59a647d940a7ad57432506bcc
Sebas Echazú <sebastianechazu@outlook.com>
2024-12-04 18:16:25 -0300

    Agregar PrimeNG y PrimeIcons, configurar el tema y actualizar el componente principal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MenuItem } from 'primeng/api';
+import { Menu } from 'primeng/menu';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Menu],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'alta-colegiado';
+export class AppComponent implements OnInit {
+
+  items: MenuItem[] | undefined;
+
+  ngOnInit() {
+    this.items = [
+      { label: 'Datos Personales', icon: 'pi pi-user' },
+      { label: 'Datos Académicos', icon: 'pi pi-book' },
+      { label: 'Dirección Profesional', icon: 'pi pi-briefcase' },
+      { label: 'Datos Bancarios', icon: 'pi pi-credit-card' },
+      { label: 'Especialidades', icon: 'pi pi-star' },
+      { label: 'Premios y distinciones', icon: 'pi pi-trophy' },
+      { label: 'Publicación de datos', icon: 'pi pi-upload' },
+      { label: 'Aceptación y Enviar', icon: 'pi pi-check' }
+    ];
+  }
+
+
 }
